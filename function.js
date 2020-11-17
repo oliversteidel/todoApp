fc2d7cb477204db70ce9d5e149d28cc31093503c
oliversteidel <67387391+oliversteidel@users.noreply.github.com>
2020-11-17 22:32:05 +0100

    major changes
    simplyfied setup for cleaner js
    layout changes: size, color

--- function.js
+++ function.js
@@ -1,36 +1,38 @@
-var taskCounter = 1;
+
 var userInput = "";
-var isValue = 0;
-var checked = '<i class="far fa-check-circle"></i>';
+var inputArr = [];
 
 $(document).ready(function () {
 
-    $('.task-input').focus();  //auto focus on element
+    $('.task-input').focus();
 
-
-    $('.input-btn').click(function () {
+    function addTask() {
         userInput = $('.task-input').val();
         $('.task-input').val("");
-        $('.task-' + taskCounter).html(userInput);
-        $('.task-wrapper-' + taskCounter).removeClass('hide');
-        taskCounter++;
-    });
+        inputArr.push(userInput);
+        $('.task-container').append('<p class="task">' + userInput + '</p>');
+    }
 
-    //toggle input button on enter-key (value=13 (event.which))
-    $('.task-input').keydown(function (event) {
-        if (event.which === 13 && $('.task-input').val() != "") {
-            userInput = $('.task-input').val();
-            $('.task-input').val("");
-            $('.task-' + taskCounter).html(userInput);
-            $('.task-wrapper-' + taskCounter).removeClass('hide');
-            taskCounter++;
+    $('.input-btn').click(function () {
+        if ($('.task-input').val() != "") {
+            addTask();
         }
     });
 
-    $('.btn-done').click(function () {
-        isValue = $(this).attr("value");
-        console.log(isValue);
-        $('.task-' + isValue).addClass('trans');
-        $('.btn-' + isValue).html(checked);
+    $(".task-input").keydown(function (event) {
+        if (event.which === 13 && $(".task-input").val() != "") {
+            addTask();
+        }
     });
+
+    $('body').on('click', '.task', function () {
+        $(this).css('opacity', '0.5');
+        $(this).addClass('checked');
+    });
+
+    $('.btn-clear-done').click(function () {
+        $('.checked').remove();
+    });
+
+
 });
